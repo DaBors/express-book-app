82ed56308c753c849c015622e3905f97202da2be
Márton Borsos <borsos.marton95@gmail.com>
2022-12-06 19:51:49 +0100

    Added test to check if the server is running properly

---
+++ tests/backend/server.test.ts
@@ -0,0 +1,16 @@
+import request from 'supertest';
+import server from '../../backend/server';
+
+describe("GET /", () => {
+
+  afterEach(async () => {
+    // Close the server instance after each test
+    await server.close()
+  })
+
+  it("check if server is running", async () => {
+    const res = await request(server).get("/");
+    expect(res.statusCode).toBe(200);
+  });
+
+});
