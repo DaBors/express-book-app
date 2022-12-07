9d471817327c89c9a2c176724fde3a717d96a22a
Márton Borsos <borsos.marton95@gmail.com>
2022-12-07 23:11:27 +0100

    Replaced single quotes with double quotes
    This is to only follow one standard without creating confusion

--- backend/api/models/user.ts
+++ backend/api/models/user.ts
@@ -1,5 +1,5 @@
-import { v4 as uuidv4 } from 'uuid';
-import AuthService from '../services/authService';
+import { v4 as uuidv4 } from "uuid";
+import AuthService from "../services/authService";
 
 export default class User {
 
--- backend/server.ts
+++ backend/server.ts
@@ -1,23 +1,23 @@
-import express from 'express';
-import http from 'http';
-import cors from 'cors';
+import express from "express";
+import http from "http";
+import cors from "cors";
 
-import debug from 'debug';
-import morgan from 'morgan';
+import debug from "debug";
+import morgan from "morgan";
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
-const port: string = process.env.PORT || '3000';
+const port: string = process.env.PORT || "3000";
 
-const debugLog: debug.IDebugger = debug('app');
+const debugLog: debug.IDebugger = debug("app"); 
 
 app.use(express.json());
 
 app.use(cors());
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.get('/', (_: express.Request, res: express.Response) => {
+app.get("/", (_: express.Request, res: express.Response) => {
     res.status(200).send(`Server is running at http://localhost:${port}`)
 });
 
