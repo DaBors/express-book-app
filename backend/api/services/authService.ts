ef733db305009cd1c74c693f2cc46398c4f27a40
Márton Borsos <borsos.marton95@gmail.com>
2022-12-07 22:58:08 +0100

    Added implementation for AuthService that verifies
    Using bcrypt for password hash creation and verification
    
    Enabled the corresponding test for AuthService

--- backend/api/services/authService.ts
+++ backend/api/services/authService.ts
@@ -1,12 +1,37 @@
+import * as bcrypt from "bcrypt";
 import User from "../models/user";
 
 export default class AuthService {
 
+    /**
+    * Creates a hash from the password provided by the user
+    *
+    * @param password - The password to create the hash from
+    * @remarks The hash contains the salt in itself, so only the hash needs to be stored
+    * 
+    * @returns The hash of the password
+    */
     static generateHash(password: string): string {
-        return "";
+        const saltRounds: number = 10;
+        const hash: string = bcrypt.hashSync(password, saltRounds);
+
+        return hash;
     }
 
+    /**
+    * Verifies if the user has provided the right password that corresponds to the stored hash
+    *
+    * @param user - The User object that represent the user who needs to be verified
+    * @param password - The password that has submitted for verification
+    * 
+    * @returns Returns true if the password matches to the hash, returns false otherwise
+    */
     static verifyUser(user: User, password: string): boolean {
+        if (bcrypt.compareSync(password, user.hash)) {
+            return true;
+        }
+
         return false;
     }
+
 }
--- tests/backend/services/authService.test.ts
+++ tests/backend/services/authService.test.ts
@@ -3,12 +3,12 @@ import AuthService from "../../../backend/api/services/authService";
 
 describe("Testing auth service functionalities", () => {
 
-    xit("can verify user with right password", async () => {
+    it("can verify user with right password", async () => {
         const marton = new User("Marton", "NotMarton", "DefinitelyNotMarton");
         expect(AuthService.verifyUser(marton, "NotMarton")).toEqual(true);
     });
 
-    xit("won't verify user with wrong password", async () => {
+    it("won't verify user with wrong password", async () => {
         const marton = new User("Marton", "NotMarton", "DefinitelyNotMarton");
         expect(AuthService.verifyUser(marton, "admin")).toEqual(false);
     });
