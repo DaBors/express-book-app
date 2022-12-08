bceb983fbef654e138f876c3308f1f1678f2bfff
Márton Borsos <borsos.marton95@gmail.com>
2022-12-08 08:49:27 +0100

    Added dataService implementation to store users

--- backend/api/services/dataService.ts
+++ backend/api/services/dataService.ts
@@ -1,16 +1,53 @@
+import * as fs from "fs";
 import User from "../models/user";
-
+import development_data from "../database/development_users.json";
+import test_data from "../database/test_users.json";
 
 class DataService {
 
-    static saveUser(user: User) {}
+    /**
+    * Saves a User in the db in case the username doesn't already exists
+    *
+    * @param user - The `user` object to be saved
+    * 
+    * @returns `true` if the user was successfully saved, returns `false` otherwise
+    */
+    static saveUser(user: User): boolean {
+        if (User.getUserBy({ username: user.username }) === undefined) {
+            users.push(user)
+            fs.writeFileSync(`./backend/api/database/${process.env.NODE_ENV}_users.json`, JSON.stringify(users, null, 2), "utf-8");
 
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+    * Returns all the users stored in the db corresponding to the current environment
+    * 
+    * @returns `User` array that holds the data of the users stored in the db
+    */
     static loadAllUsers(): User[] {
-        return [];
+        switch (process.env.NODE_ENV) {
+            case "development": {
+                return development_data as User[];
+            }
+            case "test": {
+                return test_data as User[];
+            }
+            default: {
+                return []
+            }
+        }
+
     }
 
 }
 
-var users: User[] = []
+/**
+* Stores all the `User` objects in the current environment
+*/
+var users: User[] = DataService.loadAllUsers();
 
-export {users, DataService};
+export { users, DataService };
--- tests/backend/services/dataService.test.ts
+++ tests/backend/services/dataService.test.ts
@@ -3,12 +3,11 @@ import { users, DataService } from "../../../backend/api/services/dataService";
 
 describe("Testing data service functionalities", () => {
 
-    xit("can store new user", async () => {
-        const userCountBefore = users.length;
+    it("can store new user", async () => {
         const masha = new User("Masha", "NotMasha", "DefinitelyNotMasha");
         DataService.saveUser(masha);
 
-        expect(userCountBefore).toEqual(users.length - 1);
+        expect(1).toEqual(users.length);
     });
 
 });
