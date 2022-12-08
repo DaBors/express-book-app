dc993f7f5f975db5bb31fb1843b3488d9316ba14
Márton Borsos <borsos.marton95@gmail.com>
2022-12-08 21:54:07 +0100

    Added test for /books with query params

--- tests/backend/routes/bookRoutes.test.ts
+++ tests/backend/routes/bookRoutes.test.ts
@@ -7,11 +7,16 @@ import { server } from "../../../backend/server";
 
 describe("Test all book api endpoints", () => {
     const masha = new User("Masha", "NotMasha", "DefinitelyNotMasha");
-    const mashasBook = new Book("Title", "Description", masha, "https://example.com", 200);
+    const mashasBook = new Book("MashasTitle", "MashasDescription", masha, "https://masha.com", 200);
+    
+    const marton = new User("Marton", "NotMarton", "DefinitelyNotMarton");
+    const martonsBook = new Book("MartonsTitle", "MartonsDescription", marton, "https://marton.com", 200);
 
     beforeAll(async () => {
         DataService.saveUser(masha);
         DataService.saveBook(mashasBook);
+        DataService.saveUser(marton);
+        DataService.saveBook(martonsBook);
     })
 
     afterAll(async () => {
@@ -32,6 +37,16 @@ describe("Test all book api endpoints", () => {
         const resXml= await request(server).get("/books").set("Content-Type", "text/xml");
         expect(resXml.statusCode).toBe(200);
         expect(resXml.text).toEqual(xml(JSON.stringify([mashasBook])))
+    });
+
+    xit("get all books filters", async () => {
+        const resFirst = await request(server).get("/books").query({ title: 'MartonsTitle' }).set("Content-Type", "application/json");
+        expect(resFirst.statusCode).toBe(200);
+        expect(resFirst.body).toEqual([martonsBook])
+
+        const resSecond = await request(server).get("/books").query({ description: 'MashasDescription' }).set("Content-Type", "application/json");
+        expect(resSecond.statusCode).toBe(200);
+        expect(resSecond.body).toEqual([mashasBook])
     });
 
 });
