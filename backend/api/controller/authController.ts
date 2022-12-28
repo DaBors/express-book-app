ce8511aba7389a044f00d1165518368f9ca68abc
Márton Borsos <borsos.marton95@gmail.com>
2022-12-28 20:04:05 +0100

    Added verify user jwt code

--- backend/api/controller/authController.ts
+++ backend/api/controller/authController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import User from "../models/user";
-import jsonwebtoken from "jsonwebtoken";
+import jsonwebtoken, { JwtPayload } from "jsonwebtoken";
 import AuthService from "../services/authService";
 import generateResponse from "./responseController";
 
@@ -19,4 +19,30 @@ const authenticate = (req: Request, res: Response) => {
     return generateResponse(401, { error: "Incorrect username or password" }, req, res);
 };
 
-export { authenticate };
+/**
+* Verifies the user making the api call based on the Jwt provided in the authorization header
+*
+* @param request - The request express object 
+* @param response - The response express object 
+* 
+* @returns The User object representing the authenticated user, null if the jwt couldn't be verified
+*/
+const verifyUser = (request: Request, response: Response): User | null => {
+    if (request.headers.authorization && request.headers.authorization.split(" ")[0] === "Bearer") {
+        const token = request.headers.authorization.split(' ')[1];
+        const jwt: JwtPayload = jsonwebtoken.verify(token, process.env.JWT_SECRET ?? "topSecretJwt") as JwtPayload;
+
+        const user: User | undefined = User.getUserBy({ id: jwt.userId });
+        if (user) {
+            return user;
+        }
+
+        response.sendStatus(400);
+        return null;
+    }
+
+    response.sendStatus(401);
+    return null;
+};
+
+export { authenticate, verifyUser };
